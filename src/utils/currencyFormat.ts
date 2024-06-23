0b7be9150be3f242138e1d4b99b4359709e32886
Felix <Felixsanchez73@outlook.com>
2024-06-22 23:18:09 -0400

    chore: Update currency format to use USD instead of DOP

--- src/utils/currencyFormat.ts
+++ src/utils/currencyFormat.ts
@@ -1,7 +1,7 @@
 export const currencyFormat = (value: number) => {
     return new Intl.NumberFormat('en-US', {
         style: 'currency',
-        currency: 'DOP',
+        currency: 'USD',
         maximumFractionDigits: 2,
         minimumFractionDigits: 2
     }).format(value);
