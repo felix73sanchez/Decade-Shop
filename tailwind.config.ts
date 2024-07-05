0d2f5cf3f4f916eb06b814fab0acd633ad63d69c
Deca <idelvisvelazquez@gmail.com>
2024-07-05 12:54:50 -0400

    color de la marca agregado en tailwind.config.ts

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -13,6 +13,13 @@ const config: Config = {
         "gradient-conic":
           "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
       },
+      colors: {
+        // Define tus colores personalizados aquí
+        colorGreen: '#506E42',
+        colorblanco: '#FFFDF6',
+        //colorblanco: 'rgb(34, 139, 34)',
+        //customRed: 'hsl(0, 100%, 50%)',
+      },
     },
   },
   plugins: [],
