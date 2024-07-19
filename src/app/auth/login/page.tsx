26454558d9d206c0d169cb67d1ee3eaad3408fa3
Deca <idelvisvelazquez@gmail.com>
2024-07-18 21:49:07 -0400

    page

--- src/app/auth/login/page.tsx
+++ src/app/auth/login/page.tsx
@@ -9,7 +9,7 @@ export default function LoginPage() {
           <img src="/gridimg/6.jpg" alt="Marca" className="absolute w-full h-full object-cover  rounded-brImg "/>
         </div>
         <div className="flex flex-col justify-center items-center w-full lg:w-1/2 h-full lg:h-screen ">
-          <div className="relative z-10 flex flex-col justify-center items-center -top-12 w-full">
+          <div className="relative z-10 flex flex-col justify-center items-center -top-12 XR:-top-8 12Pro:-top-8 14Pro:-top-20 14Pro:text-fs1 w-full">
             <h1 className={`${logoFont.className} font-fw9 italic text-5xl text-colorPrimary `}>DECAVENTURE</h1>
           </div>
           <LoginForm />
