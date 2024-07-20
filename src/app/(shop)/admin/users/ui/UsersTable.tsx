cff0734d9deec885710181eb77d38031319320db
Felix Sanchez <felixsanchez73@outlook.com>
2024-07-19 22:50:03 -0400

    refactor: Add user role change functionality to UsersTable component

--- src/app/(shop)/admin/users/ui/UsersTable.tsx
+++ src/app/(shop)/admin/users/ui/UsersTable.tsx
@@ -2,12 +2,24 @@
 
 import { changeUserRole } from "@/actions";
 import { User } from "@/interfaces";
+import { useState } from 'react';
 
 interface Props {
     users: User[];
 }
 
 export const UsersTable = ({ users }: Props) => {
+    const [updatedUsers, setUpdatedUsers] = useState(users);
+
+    const handleChangeRole = async (userId: string, newRole: string) => {
+        await changeUserRole(userId, newRole);
+        setUpdatedUsers((prevUsers) =>
+            prevUsers.map((user) =>
+                user.id === userId ? { ...user, role: newRole } : user
+            )
+        );
+    };
+
     return (
         <table className="min-w-full">
             <thead className="bg-gray-200 border-b">
@@ -30,11 +42,10 @@ export const UsersTable = ({ users }: Props) => {
                     >
                         Rol
                     </th>
-
                 </tr>
             </thead>
             <tbody>
-                {users.map((user) => (
+                {updatedUsers.map((user) => (
                     <tr
                         key={user.id}
                         className="bg-white border-b transition duration-300 ease-in-out hover:bg-gray-100"
@@ -45,21 +56,19 @@ export const UsersTable = ({ users }: Props) => {
                         <td className="text-sm text-gray-900 font-light px-6 py-4 whitespace-nowrap">
                             {user.name}
                         </td>
-                        <td className="flex items-center text-sm  text-gray-900 font-light px-6 py-4 whitespace-nowrap">
+                        <td className="flex items-center text-sm text-gray-900 font-light px-6 py-4 whitespace-nowrap">
                             <select
                                 value={user.role}
-                                onChange={(e) => changeUserRole(user.id, e.target.value)}
-                                className=" text-sm w-full p-2 text-gray-900 ">
+                                onChange={(e) => handleChangeRole(user.id, e.target.value)}
+                                className="text-sm w-full p-2 text-gray-900"
+                            >
                                 <option value="admin">Administrador</option>
                                 <option value="user">Usuario</option>
                             </select>
                         </td>
-
                     </tr>
                 ))}
-
-
             </tbody>
         </table>
-    )
-}
+    );
+};
