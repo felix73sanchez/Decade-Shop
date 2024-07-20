cf84d018d6946c9f3354ce70bc3079d512bae724
Felix Sanchez <felixsanchez73@outlook.com>
2024-07-19 21:37:46 -0400

    refactor: agregue carga de diferentes lugares para imágenes de productos en el componente ProductGridItem

--- src/components/products/product-grid/ProductGridItem.tsx
+++ src/components/products/product-grid/ProductGridItem.tsx
@@ -1,53 +1,70 @@
 'use client';
 
-import { allFont } from "@/config/fonts"
-import { PiHeartBold } from "react-icons/pi";
 import { Product } from "@/interfaces";
 import Image from "next/image";
 import Link from "next/link";
-import { useState } from "react";
-
+import { useState, useEffect } from "react";
 
 interface Props {
     product: Product;
 }
 
 export const ProductGridItem = ({ product }: Props) => {
+    const [imageUrls, setImageUrls] = useState<string[]>([]);
+    const [displayImage, setDisplayImage] = useState<string>('');
 
-    const [displayImage, setDisplayImage] = useState(product.images[0]);
+    useEffect(() => {
+        const checkLocalImages = async () => {
+            const localImageUrls = await Promise.all(
+                product.images.map(async (image) => {
+                    const localUrl = `/products/${image}`;
+                    try {
+                        const response = await fetch(localUrl, { method: 'HEAD' });
+                        if (response.ok) {
+                            return localUrl;
+                        } else {
+                            return image; // URL de Cloudinary
+                        }
+                    } catch (error) {
+                        return image; // URL de Cloudinary
+                    }
+                })
+            );
+            setImageUrls(localImageUrls);
+            setDisplayImage(localImageUrls[0]); // Establece la imagen inicial
+        };
 
+        checkLocalImages();
+    }, [product.images]);
+
+    if (imageUrls.length === 0) {
+        return null; // Evita renderizar el componente si no se han cargado las URLs
+    }
 
     return (
-        <div className={`${allFont.className} p-p8 overflow-hidden fade-in bg-colorPrima rounded-brAll shadow-customBS border-customBC border-customBW `}>
-            
-            <Link href={`/product/${product.slug}`} >
+        <div className="p-0 overflow-hidden fade-in bg-colorSecondary rounded-brAll shadow-customBS border-customBC border-customBW">
+            <Link href={`/product/${product.slug}`}>
                 <Image
-                    src={`/products/${displayImage}`}
+                    src={displayImage}
                     alt={product.title}
-                    className="w-full object-cover rounded-brAll shadow-custom-2 "
+                    className="w-full object-cover border-customBC border-b-customBW"
                     priority
                     width={852}
                     height={852}
-                    onMouseEnter={() => setDisplayImage(product.images[1])}
-                    onMouseLeave={() => setDisplayImage(product.images[0])}
-                 />
+                    onMouseEnter={() => setDisplayImage(imageUrls[1])}
+                    onMouseLeave={() => setDisplayImage(imageUrls[0])}
+                />
             </Link>
-            
-            <div className="flex justify-between items-start w-full h-fit py-2 px-4  text-colorPrimary gap-g8">
-                <div className="flex-1 flex flex-col">
-                    <Link 
-                        className=" hover:text-colorHover text-fs1 font-fw9 pt-1"
-                        href={`/producs/${product.slug}`}
-                        >{product.title}</Link>
-                    <span className="font-fw4 text-fs1 break-words">${product.price}</span>
-                </div>
-                <div className=" flex-shrink-0 mt-auto pt-6  ">
-                    <button className="bottom-0" >
-                    <PiHeartBold className="text-3xl w-8 h-8 p-1 rounded-lg bg-color4 text-colorSecondary hover:text-color3  shadow-custom-2"/>
-                    </button>
-                </div>                
-            </div>
 
+            <div className="p-2 m-0 flex flex-col text-colorPrimary">
+                <Link
+                    className="hover:text-color text-fs1 font-fw7"
+                    href={`/product/${product.slug}`}
+                >
+                    {product.title}
+                </Link>
+                <span className="font-fw4 text-fs1">${product.price}</span>
+            </div>
         </div>
-    )
-}
+    );
+};
--- src/components/ui/gridimg/GridImg.tsx
+++ src/components/ui/gridimg/GridImg.tsx
@@ -8,9 +8,9 @@ interface Props {
 }
 
 const images = [
-    { src: '/gridimg/1.jpg', alt: 'Descripción de la imagen izquierda' },
-    { src: '/gridimg/2.jpg', alt: 'Descripción de la imagen derecha superior' },
-    { src: '/gridimg/3.jpg', alt: 'Descripción de la imagen derecha inferior' }
+    { src: '/gridimg/1.jpg', alt: 'Descripción de la imagen izquierda', width: 600, height: 400 },
+    { src: '/gridimg/2.jpg', alt: 'Descripción de la imagen derecha superior', width: 600, height: 400 },
+    { src: '/gridimg/3.jpg', alt: 'Descripción de la imagen derecha inferior', width: 600, height: 400 }
 ];
 
 export const GridImg: React.FC<Props> = ({ className }) => {
@@ -19,7 +19,7 @@ export const GridImg: React.FC<Props> = ({ className }) => {
     useEffect(() => {
         const interval = setInterval(() => {
             setCurrentImageIndex((prevIndex) => (prevIndex + 1) % images.length);
-        }, 5000); // Cambiar imagen cada 3 segundos
+        }, 5000); // Cambiar imagen cada 5 segundos
         return () => clearInterval(interval);
     }, []);
 
@@ -27,7 +27,13 @@ export const GridImg: React.FC<Props> = ({ className }) => {
         <div className={`relative w-full h-[36rem] px-p8 overflow-hidden fade-in mt-[4.6rem] grid grid-cols-1 sm:grid-cols-3 gap-p8 sm:gap-g8 ${allFont.className} ${className}`}>
             {/* Imagen visible en pantallas móviles */}
             <div className="relative w-full h-[36rem] sm:hidden rounded-brAll shadow-customBS border-customBC border-customBW">
-                <Image src={images[currentImageIndex].src} alt={images[currentImageIndex].alt} className="w-full h-full object-cover rounded-brAll" />
+                <Image
+                    src={images[currentImageIndex].src}
+                    alt={images[currentImageIndex].alt}
+                    width={images[currentImageIndex].width}
+                    height={images[currentImageIndex].height}
+                    className="w-full h-full object-cover rounded-brAll"
+                />
                 <div className="absolute inset-x-0 bottom-4 flex justify-center">
                     {images.map((image, index) => (
                         <div key={index} className={`w-2 h-2 rounded-full mx-1 ${index === currentImageIndex ? 'bg-color3' : 'bg-color4'}`} />
@@ -41,7 +47,13 @@ export const GridImg: React.FC<Props> = ({ className }) => {
             {/* Div a la izquierda, visible en pantallas medianas y grandes */}
             <div className="relative hidden sm:block w-full h-[36rem] sm:col-span-2 rounded-brAll shadow-customBS border-customBC border-customBW">
                 <div className="w-full h-full overflow-hidden rounded-brAll">
-                    <Image src="/gridimg/1.jpg" alt="Descripción de la imagen izquierda" className="w-full h-full object-cover" />
+                    <Image
+                        src="/gridimg/1.jpg"
+                        alt="Descripción de la imagen izquierda"
+                        width={800}
+                        height={600}
+                        className="w-full h-full object-cover"
+                    />
                 </div>
                 <div className="absolute top-4 left-4 sm:top-8 sm:left-10">
                     <p className="text-colorPrimary text-xl sm:text-5xl font-fw9">NEW ARRIVALS</p>
@@ -51,13 +63,25 @@ export const GridImg: React.FC<Props> = ({ className }) => {
             {/* Contenedor de los dos divs a la derecha, visible en pantallas medianas y grandes */}
             <div className="relative hidden sm:grid grid-rows-1 gap-g8 sm:gap-g8 w-full h-full">
                 <div className="relative w-full h-[17.75rem] rounded-brAll shadow-customBS border-customBC border-customBW">
-                    <Image src="/gridimg/2.jpg" alt="Descripción de la imagen derecha superior" className="w-full h-full object-cover rounded-brAll" />
+                    <Image
+                        src="/gridimg/2.jpg"
+                        alt="Descripción de la imagen derecha superior"
+                        width={800}
+                        height={600}
+                        className="w-full h-full object-cover rounded-brAll"
+                    />
                     <div className="absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 p-p8">
                         {/* <p className="text-colorSecondary text-xl sm:text-3xl font-fw9 whitespace-nowrap">YOUR STORY</p> */}
                     </div>
                 </div>
                 <div className="relative w-full h-[17.75rem] bg-colorSecondary rounded-brAll shadow-customBS border-customBC border-customBW">
-                    <Image src="/gridimg/3.jpg" alt="Descripción de la imagen derecha inferior" className="w-full h-full object-cover rounded-brAll" />
+                    <Image
+                        src="/gridimg/3.jpg"
+                        alt="Descripción de la imagen derecha inferior"
+                        width={800}
+                        height={600}
+                        className="w-full h-full object-cover rounded-brAll"
+                    />
                     <div className="absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2">
                         {/* <p className="text-colorSecondary text-xl sm:text-3xl font-fw9 whitespace-nowrap">ANYTIME</p> */}
                     </div>
