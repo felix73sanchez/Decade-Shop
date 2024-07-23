6e501ff1fc5b226158154b25a589378171b2e937
Felix Sanchez <111938139+felix73sanchez@users.noreply.github.com>
2024-07-23 13:46:13 -0400

    Rollback: Ajuste de imagen

--- src/components/products/product-grid/ProductGridItem.tsx
+++ src/components/products/product-grid/ProductGridItem.tsx
@@ -26,8 +26,8 @@ export const ProductGridItem = ({ product }: Props) => {
                     alt={product.title}
                     className="w-full object-cover rounded-brAll shadow-custom-2 "
                     priority
-                    width={200}
-                    height={200}
+                    width={850}
+                    height={850}
                     onMouseEnter={() => setDisplayImage(product.images[1])}
                     onMouseLeave={() => setDisplayImage(product.images[0])}
                  />
