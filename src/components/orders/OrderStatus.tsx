d716b0d430d85ded83a992af24769d2fee694f65
Deca <159152771+HeadCreator29@users.noreply.github.com>
2024-08-01 13:59:17 -0400

    Actualizar OrderStatus.tsx

--- src/components/orders/OrderStatus.tsx
+++ src/components/orders/OrderStatus.tsx
@@ -11,14 +11,14 @@ export const OrderStatus = ({ isPaid }: Props) => {
     return (
         <div className={
             clsx(
-                "flex items-center rounded-lg py-2 px-3.5 text-xs font-bold text-white mb-5",
+                "flex items-center text-red-600",
                 {
                     'bg-red-500': !isPaid,
                     'bg-green-600': isPaid,
                 }
             )
         }>
-            <IoCardOutline size={30} className="mr-2" />
+            <IoCardOutline size={30} className="mr-1" />
             {/* <span className="mx-2 "> Pendiente de pago </span> */}
             <span className="mx-2 ">
                 {
